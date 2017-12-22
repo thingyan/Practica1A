1a8cea679b11521e3b272c18092ea19310a62b76
mk929 <myo_k@hotmail.com>
2017-12-22 01:53:25 -0500

    // branch_2 add practica1.data

---
+++ Practica1.Data/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1.Data
+{
+    // branch_2 add practica1.data
+    public class Class1
+    {
+    }
+}
